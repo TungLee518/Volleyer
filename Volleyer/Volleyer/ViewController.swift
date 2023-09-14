240d937897b62c21af3b01b8f0e105ead9fd495a
TungLee518 <maymmm518@gmail.com>
2023-09-14 13:41:47 +0800

    add firebase

--- Volleyer/Volleyer/ViewController.swift
+++ Volleyer/Volleyer/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
+    
+    var db: Firestore!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func addTestDataToFirebase() {
+        let settings = FirestoreSettings()
+
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("tests").addDocument(data: [
+            "first": "May",
+            "middle": "",
+            "last": "Lee",
+            "born": 1999
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
     }
 
 
